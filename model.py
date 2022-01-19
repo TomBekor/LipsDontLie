054421dbe49493dc645c8c75aac2c8a28e10eb00
TomBekor <tom.bekor@gmail.com>
2022-01-19 19:04:58 +0200

    positional encoding

--- model.py
+++ model.py
@@ -37,6 +37,7 @@ class Transformer(nn.Module):
     def __init__(self, target_size: int, d_model: int = TRANSFORMER_D_MODEL, num_heads: int = TRANSFORMER_N_HEADS):
         super(Transformer, self).__init__()
         self.d_model = d_model
+        self.pos_encoder = PositionalEncoding(d_model)
         self.embedding = nn.Embedding(target_size, d_model)
         self.transformer = nn.Transformer(d_model=d_model, batch_first=True, nhead=num_heads)
         self.generator = nn.Linear(d_model, target_size)
@@ -54,3 +55,21 @@ class Transformer(nn.Module):
             src_key_padding_mask=batch_in_pad_masks, tgt_key_padding_mask=batch_tgt_pad_masks)
         outs = self.generator(outs)
         return outs
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
