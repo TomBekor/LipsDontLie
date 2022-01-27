0f140523343df166c00d3a5ab50565f85b2ac21f
TomBekor <tom.bekor@gmail.com>
2022-01-27 17:08:42 +0200

    config

--- config.py
+++ config.py
@@ -1,20 +1,22 @@
-''' A configuration file which contains constants
- and hyperparameters used in our project'''
+'''
+A configuration file which contains constants
+and hyperparameters used in our project
+'''
 
 # Paths
 VOCABULARY_FILE = 'vocab.txt'
-VIDEOS_DIR = 'videos'
-ANNOTATIONS_DIR = 'npy_alignments'
-LANDMARKS_DIR = 'npy_landmarks'
+VIDEOS_DIR = './videos'
+ORIGINAL_ALIGNMENTS_DIR = './alignments'
+ANNOTATIONS_DIR = './npy_alignments'
+LANDMARKS_DIR = './npy_landmarks'
 
 # Preprocessing
 PRETRAINED_SHAPE_DETECTOR_PATH = './facial-landmarks-models/shape_predictor_68_face_landmarks.dat'
 
 # DataLoader
-SPEAKERS = list(range(2,6)) + list(range(22,29))
-# SPEAKERS = list(range(2,6))
-# SPEAKERS = [2]
+SPEAKERS = list(range(6,16))
 WINDOW_SIZE = 5
+MIN_FRAMES = 70
 
 # Tokenizer
 SEQUENCE_IN_MAX_LEN = 80
@@ -34,10 +36,15 @@ TRANSFORMER_DIM_FEEDFORWARD = 2048
 TRANSFORMER_DROPOUT = 0.1
 
 # Training
-BATCH_SIZE = 32
+BATCH_SIZE = 64
 EPOCHS = 60
 VERBOSE_ITERS=100
+MAX_RANDOM_FLIP_PROB = 0.3
 
 # trained models
-TRANSFORMER_PATH = 'trained_models/transformer'
-LANDMARKSNN_PATH = 'trained_models/landmarks'
+TRANSFORMER_BEST = 'final_models/trained_models_90/transformer'
+LANDMARKSNN_BEST = 'final_models/trained_models_90/landmarks'
+TRANSFORMER_SAVE = 'trained_models/transformer'
+LANDMARKSNN_SAVE = 'trained_models/landmarks'
+TRANSFORMER_LOAD = TRANSFORMER_SAVE
+LANDMARKSNN_LOAD = LANDMARKSNN_SAVE
